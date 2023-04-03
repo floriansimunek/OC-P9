d59647097cf0a7ae29318431e0dce865ea138ac1
Florian SIMUNEK <florian.simunek@gmail.com>
2023-04-03 22:29:41 +0200

    :white_check_mark: corrupted data test

--- frontend/src/__tests__/Bills.js
+++ frontend/src/__tests__/Bills.js
@@ -110,6 +110,44 @@ describe("Given I am connected as an employee", () => {
 				expect(getBills).toHaveBeenCalled();
 				expect(result.length).toBe(4);
 			});
+
+			test("log the error and return unformatted date in the case of corrupted data", async () => {
+				const bill = [
+					{
+						id: "47qAXb6fIm2zOKkLzMro",
+						vat: "80",
+						fileUrl: "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+						status: "pending",
+						type: "Hôtel et logement",
+						commentary: "séminaire billed",
+						name: "encore",
+						fileName: "preview-facture-free-201801-pdf-1.jpg",
+						date: "2004-04-32",
+						amount: 400,
+						commentAdmin: "ok",
+						email: "a@a",
+						pct: 20,
+					},
+				];
+				const consoleSpy = jest.spyOn(console, "log");
+				const corruptedBill = {
+					bills() {
+						return {
+							list() {
+								return Promise.resolve(bill);
+							},
+						};
+					},
+				};
+				const newBill = new Bills({
+					document,
+					onNavigate: null,
+					store: corruptedBill,
+					localStorage: window.localStorage,
+				});
+				const result = await newBill.getBills();
+				expect(consoleSpy).toHaveBeenCalled();
+			});
 		});
 	});
 });
