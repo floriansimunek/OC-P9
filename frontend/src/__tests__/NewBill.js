0cc07212a69473247e56a80e7af0adaa8add1e0c
Florian SIMUNEK <florian.simunek@gmail.com>
2023-04-07 01:06:01 +0200

    :white_check_mark: Add test for submit bill if correctly fields

--- frontend/src/__tests__/NewBill.js
+++ frontend/src/__tests__/NewBill.js
@@ -5,6 +5,7 @@ import { fireEvent, screen } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 import { localStorageMock } from "../__mocks__/localStorage";
 import mockStore from "../__mocks__/store.js";
@@ -27,7 +28,7 @@ describe("Given I am connected as an employee", () => {
 			jest.restoreAllMocks();
 		});
 
-		test("change file handler should display the file name if the file format is supported", () => {
+		test("Then change file handler should display the file name if the file format is supported", () => {
 			const onNavigate = (pathname) => {
 				document.body.innerHTML = ROUTES({ pathname });
 			};
@@ -47,6 +48,54 @@ describe("Given I am connected as an employee", () => {
 			expect(input.files.length).toBe(1);
 			expect(input.files[0].name).toBe("test.png");
 			expect(handleChangeFile).toHaveBeenCalledTimes(2);
+		});
+
+		test("Then it should submit new bill when I fill correctly fields", async () => {
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+			const newBill = new NewBill({
+				document,
+				onNavigate,
+				store: mockStore,
+				localStorage: window.localStorage,
+			});
+			const handleSubmit = jest.fn(newBill.handleSubmit);
+			const form = screen.getByTestId("form-new-bill");
+			form.addEventListener("submit", handleSubmit);
+			const uint8 = new Uint8Array([0xff, 0xd8, 0xff]); // JPEG signature
+			const file = new File([uint8], "test.jpeg", { type: "image/jpeg" });
+			const input = screen.getByTestId("file");
+			input.addEventListener("change", newBill.handleChangeFile);
+			fireEvent.change(input, {
+				target: {
+					files: [file],
+				},
+			});
+			fireEvent.change(screen.getByTestId("expense-name"), {
+				target: { value: "test" },
+			});
+			fireEvent.change(screen.getByTestId("datepicker"), {
+				target: { value: "2021-03-01" },
+			});
+			fireEvent.change(screen.getByTestId("amount"), {
+				target: { value: "100" },
+			});
+			fireEvent.change(screen.getByTestId("vat"), {
+				target: { value: "20" },
+			});
+			fireEvent.change(screen.getByTestId("pct"), {
+				target: { value: undefined },
+			});
+			fireEvent.change(screen.getByTestId("commentary"), {
+				target: { value: "test" },
+			});
+			newBill.fileName = "test.jpeg";
+			newBill.fileUrl = "http://localhost:3000/test.jpeg";
+			const submitButton = screen.getByText("Envoyer");
+			submitButton.addEventListener("click", handleSubmit);
+			fireEvent.submit(form);
+			expect(handleSubmit).toBeCalledTimes(1);
 		});
 	});
 });
